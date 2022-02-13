3fbcaf1a2078f7aaaaef44015f796b3dff5fee08
admin <kraiden230@gmail.com>
2022-02-13 12:04:42 +0200

    up

--- src/test.java
+++ src/test.java
@@ -4,21 +4,24 @@ import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name");
+        System.out.println("Введите имя");
 
             String name = scanner.next();
             if (name.length() < 3) {
                 System.out.println("Имя должно сожерожать от 3х символов");
             } else {
                 System.out.println("Введите ваш возвраст");
+                try {
 
-                int age = scanner.nextInt();
+                    int age = scanner.nextInt();
                     if (age > 18 & age < 50) {
                         System.out.println("Добро пожаловать " + name);
                     } else {
                         System.out.println(name + " ваш возвраст должен быть от 18 до 50 лет");
                     }
-
+                } catch (InputMismatchException t) {
+                    System.out.println("Принимаются только целочисленные значения");
+                }
             }
             }
         }
