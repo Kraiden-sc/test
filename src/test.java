405de45df51eca66e3e0952805ae4b4e29b451b9
admin <kraiden230@gmail.com>
2022-05-02 20:27:36 +0300

    up

--- src/test.java
+++ src/test.java
@@ -7,14 +7,14 @@ public class test {
         System.out.println("Введите имя");
 
             String name = scanner.next();
-            if (name.length() < 4) {
+            if (name.length() < 3) {
                 System.out.println("Имя должно сожерожать от 3х символов");
             } else {
                 System.out.println("Введите ваш возвраст");
                 try {
 
                     int age = scanner.nextInt();
-                    if (age > 18 & age < 50) {
+                    if (age >= 18 & age < 50) {
                         System.out.println("Добро пожаловать " + name);
                     } else {
                         System.out.println(name + " ваш возвраст должен быть от 18 до 50 лет");
