cae419b49327d51391e32035c025ef918efe683b
admin <kraiden230@gmail.com>
2022-08-08 20:32:56 +0300

    up

--- src/test.java
+++ src/test.java
@@ -14,10 +14,10 @@ public class test {
                 try {
 
                     int age = scanner.nextInt();
-                    if (age > 18 & age < 50) {
+                    if (age > 17 & age < 50) {
                         System.out.println("Добро пожаловать " + name);
                     } else {
-                        System.out.println(name + " ваш возвраст должен быть от 18 до 50 лет");
+                        System.out.println(name + " ваш возвраст должен быть от 18 до 50 лет включительно");
                     }
                 } catch (InputMismatchException t) {
                     System.out.println("Принимаются только целочисленные значения");
