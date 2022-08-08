930ddc89bee6df7f366b2f579e0d0aef1133a395
admin <kraiden230@gmail.com>
2022-08-08 17:04:25 +0300

    up

--- src/test.java
+++ src/test.java
@@ -7,7 +7,7 @@ public class test {
         System.out.println("Введите имя");
 
             String name = scanner.next();
-            if (name.length() < 2) {
+            if (name.length() < 3) {
                 System.out.println("Имя должно состоять минимум из 3х символов");
             } else {
                 System.out.println("Введите ваш возвраст");
