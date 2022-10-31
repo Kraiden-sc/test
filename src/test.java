0bf599d3e36092c6a5cf0eb10b684b469037c70a
admin <kraiden230@gmail.com>
2022-10-31 19:56:02 +0200

    up

--- src/test.java
+++ src/test.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите имя");
+        System.out.println("Введите имя!");
 
             String name = scanner.next();
-            if (name.length() < 2) {
-                System.out.println("Имя должно состоять минимум из 3х символов");
+            if (name.length() < 3) {
+                System.out.println("Имя должно состоять минимум из 3х символов!");
             } else {
                 System.out.println("Введите ваш возвраст");
                 try {
 
                     int age = scanner.nextInt();
-                    if (age > 17 & age < 50) {
+                    if (age > 18& age < 50) {
                         System.out.println("Добро пожаловать " + name);
                     } else {
                         System.out.println(name + " ваш возвраст должен быть от 18 до 50 лет включительно");
